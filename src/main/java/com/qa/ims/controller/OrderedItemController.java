d8251790e57d702f03c1fd569258435132dc0ef1
jt1001 <jovanlana@hotmail.co.uk>
2022-02-18 18:50:33 +0000

    ordereditems current

---
+++ src/main/java/com/qa/ims/controller/OrderedItemController.java
@@ -0,0 +1,41 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.OrderedItem;
+import com.qa.ims.utils.Utils;
+
+public class OrderedItemController implements CrudController<OrderedItem>{
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private OrderedItemDAO orderedItemDAO;
+	private Utils utils;
+	
+	@Override
+	public List<OrderedItem> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public OrderedItem create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public OrderedItem update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	} 
+	
+
+}
